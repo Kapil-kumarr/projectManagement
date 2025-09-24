47edd65c3ae409df39d2524f172239c096792eba
kapil kumar <kumar.kapil0905@gmail.com>
2025-09-24 21:13:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
