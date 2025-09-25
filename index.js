826aa0a51c8f31252ad85868a9cd1781f1bc1376
kapil kumar <kumar.kapil0905@gmail.com>
2025-09-25 16:45:36 +0530

    adding healthcheckup route

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import { connectDB } from "./db/database.js";
 dotenv.config({
   path: "./.env",
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Mongo DB connection failed, Error: ", error);
+    process.exit(1);
+  });
